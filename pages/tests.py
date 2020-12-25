4810d9dc3ff6ad3c4b6b2d3a2e65cfadc2eb4dc3
Ky Nguyen <kynguyen@Kys-MacBook-Pro.local>
2020-12-26 00:24:07 +0700

    Add project

---
+++ pages/tests.py
@@ -0,0 +1,45 @@
+from django.test import TestCase, SimpleTestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class HomePageTest(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
+
+class SignUpPagetest(TestCase):
+    username = 'newuser'
+    email = 'newuser@email.com'
+
+    def test_signup_page_status_code(self):
+        response = self.client.get('/users/signup')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_users_correct_template(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'signup.html')
+
+    def test_signup_form(self):
+        new_user = get_user_model().objects.create_user(
+            self.username, self.email)
+
+        allUsers = get_user_model().objects.all()
+        self.assertEqual(allUsers.count(), 1)
+        self.assertEqual(allUsers[0].username, self.username)
+        self.assertEqual(allUsers[0].email, self.email)
